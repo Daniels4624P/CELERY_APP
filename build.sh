955d725c2d33b019fb609b3350eacb1e9567b35a
Daniel <96382302+Daniels4624P@users.noreply.github.com>
2024-12-11 17:25:00 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,17 @@ set -o errexit
 
 pip install -r requirements.txt
 
+cd theme/static_src
+npm install
+npm run build
+cd ..
+cd ..
+
 python manage.py collectstatic --noinput
+python manage.py makemigrations
 python manage.py migrate
 python manage.py populate_tasks
-python manage.py populate_proyects
+python manage.py populate_proyetcs
 python manage.py tailwind build
-celery -A Tareas_Casa worker --loglevel=info
-celery -A Tareas_Casa beat --loglevel=info
 python manage.py migrate django_celery_beat
+celery -A Tareas_Casa worker --loglevel=info
